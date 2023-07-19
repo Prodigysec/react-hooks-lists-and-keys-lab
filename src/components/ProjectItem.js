4159acf4e44927f9a25b3c120ec38b14531c0def
Ian Raburu <williamraburu@gmail.com>
2023-07-19 03:28:41 -0700

    Completed assignment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const technologiesElement = technologies.map((technology,index) =>{
+    return (
+      <span key={index}>{technology}</span>
+    )
+  })
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {/* render a <span> for each technology in the technologies array */
+        technologiesElement}
       </div>
     </div>
   );
